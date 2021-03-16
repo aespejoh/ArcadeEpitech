1ae457ab1bf4887f0192311f0bc599efcea24b1e
Arnau Espejo <arnaueh@gmail.com>
2021-03-16 15:28:12 +0100

    fixed compilation names

--- core/core.cpp
+++ core/core.cpp
@@ -13,10 +13,15 @@ void Core::loadlib(const std::string& lib_path)
     typedef IDisplayModule* (*fptr)();
     fptr func;
     void *handle = dlopen(lib_path.c_str(), RTLD_LAZY);
-    if (handle == nullptr)
+    if (!handle) {
+        std::cout << dlerror() << std::endl;
         return;
+    }
+    dlerror();
     func = (fptr)dlsym(handle, "create");
     lib = (IDisplayModule*)func();
+    if (dlclose(handle) != 0)
+        exit(84);
     _libs.push_back(lib);
 }
 
